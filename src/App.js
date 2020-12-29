4a073d3df62ede2d97044db7a8fd284bfe599a05
Yerim <yerimchoi115@gmail.com>
2020-12-29 11:53:15 +0900

    day1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+const LanguageILike = [
+  {
+    Id:1,
+    name : "cplusplus",
+    image : "https://www.flaticon.com/svg/static/icons/svg/919/919841.svg",
+    rating : 5.0
+  },
+  {
+    Id : 2,
+    name : "python",
+    image : "https://www.flaticon.com/svg/static/icons/svg/919/919852.svg",
+    rating : 4.9
+  },
+  {
+    Id : 3,
+    name : "javascript",
+    image :"https://www.flaticon.com/svg/static/icons/svg/919/919828.svg",
+    rating :  4.8
+  }
+]
+
+function Languages({name, img, rat})
+{
+  return (
+    <div>
+      <h3>I Like {name}</h3>
+      <h4>{rat}/5.0</h4>
+      <img src={img} alt="이미지 대신 삽입 되는 문구(시각장애인용)" width="150px" height="150px"></img>
+    </div>
+  );
+}
+
+Languages.propTypes={
+  name : PropTypes.string.isRequired,
+  img : PropTypes.string.isRequired,
+  rat : PropTypes.number
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>{LanguageILike.map(Language=><Languages key={Language.Id} name={Language.name} img={Language.image} rat={Language.rating} />)}</div>
   );
 }
 
